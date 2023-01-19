41a7cfa1841ef5228d60717dcfa9b9068904cf97
sj364 <sj364@LAPTOP-UPRJBTHG>
2023-01-19 15:29:33 +0530

    add2class

---
+++ src/test/java/Practice_Maven/Intri11.java
@@ -0,0 +1,15 @@
+package Practice_Maven;
+
+import org.testng.annotations.Test;
+
+public class Intri11 {
+
+
+		
+		@Test
+		public void introducenew() {
+			System.out.println("hello sir I am a siddddd");
+		}
+
+	}
+
---
+++ src/test/java/Practice_Maven/Intro.java
@@ -0,0 +1,12 @@
+package Practice_Maven;
+
+import org.testng.annotations.Test;
+
+public class Intro {
+	
+	@Test
+	public void introduce() {
+		System.out.println("hello sir I am a newbie");
+	}
+
+}
